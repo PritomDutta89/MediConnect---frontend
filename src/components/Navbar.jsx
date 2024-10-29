f351a9392c8c2e8e63483a6d846e26e66997c413
Pritom Dutta <pritom@ayekart.com>
2024-10-30 00:49:49 +0530

    Fixed bg color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const navigate = useNavigate();
 
   return (
-    <nav className="bg-white border-gray-200 ">
+    <nav className="bg-gray-50 border-gray-200 ">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a
           href="https://flowbite.com/"
@@ -63,10 +63,10 @@ const Navbar = () => {
         <div
           className={`items-center justify-between ${
             toggle ? "" : "hidden"
-          }  w-full md:flex md:w-auto md:order-1`}
+          }  w-full md:flex md:w-auto md:order-1 `}
           id="navbar-cta"
         >
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white ">
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 ">
             <li>
               <p
                 className={`cursor-pointer block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent ${
